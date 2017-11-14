8177b7bf59bc77a84133db37f1c03edd048ed692
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-11-14 10:30:06 +0100

    Add Doc and some fix

--- mask-highlighter.js
+++ mask-highlighter.js
@@ -1,19 +1,110 @@
 import {html, render} from './node_modules/lit-html/lit-html.js';
+/**
+ * @typedef {Object} Area
+ * @property {string} height : the height of rectangle
+ * @property {string} width : the width of rectangle
+ * @property {string} top : the top position of the rectangle in mask
+ * @property {string} left : the left position of the rectangle in mask
+ *
+ * @typedef {Object} Position
+ * @property {number} line : the number of the line (according to LINE_HEIGHT parameter)
+ * @property {number} nbLines : the number of lines of the mask
+ * @property {number} col : the number of the column (according to COL_WIDTH parameter)
+ * @property {number} nbCols : the number of columns of the mask
+ * @property {string} topMargin : the top margin position of the rectangle in mask
+ * @property {string} leftMargin : the left margin position of the rectangle in mask
+ */
 
 
-
-// Extend Polymer.Element base class
+/**
+ * Class representing a Mask in webComponent
+ *
+ * It will expose properties :
+ *  * {Area} area : the area of the rectangle of the mask
+ *  * {Position} position : the position of the rectangle of the mask
+ *  * {string} lineHeight : the height of a line if you want to play with lines and cols (Default value is 1.15em)
+ *  * {string} colWidth : the width of a column if you want to play with lines and cols (Default value is 35px)
+ *
+ * It will expose thoses attributes :
+ * Area Attributes
+* * {string} height : the height of rectangle to apply to area
+* * {stirng} width : the width of rectangle to apply to area
+* * {string} top : the top position of rectangle to apply to area
+* * {string} left : the left position of rectangle to apply to area
+* Position Attributes
+* * {number} line : the line number where the rectangle will start
+* * {number} nbLines : the number of line of the rectangle
+* * {number} col : the col number where the rectangle will start
+* * {number} nbCols : the number of column of the rectangle
+* * {string} topMargin : the top position before starting counting lines
+* * {string} leftMargin : the left position before starting counting columns
+* Parameters Attributes
+* * {string} lineHeight : the height of a line if you want to play with lines and cols (Default value is 1.15em)
+* * {string} colWidth : the width of a column if you want to play with lines and cols (Default value is 35px)
+*/
 export class MaskHighlighter extends HTMLElement {
+	/**
+	 * The name of custom tag for the component
+	 */
 	static get is() {
 		return 'mask-highlighter'
 	}
 
+	/**
+	 * Get the area
+	 * @return {Area} the area
+	 */
 	get area() { return this._area;}
+	/**
+	 * Get the area
+	 * @param {Area} the area to set
+	 */
 	set area(value) {this._area = value; this._updateArea(this._area);};
 
+	/**
+	 * Get the position
+	 * @return {Position} the position
+	 */
 	get position() {return this._position;}
+	/**
+	 * Set the position
+	 * @param {Position} the position to set
+	 */
 	set position(value) {this._position = value; this._updatePosition(this._position)}
 
+	/**
+	 * Get the lineHeight
+	 * @return {string} the lineHeight
+	 */
+	get lineHeight(){return this._lineHeight;}
+	/**
+	 * @param {string} value : the lineHeight
+	 */
+	set lineHeight(value){
+		if (this._lineHeight === value) return;
+		this._lineHeight = value;
+		this.setAttribute('lineHeight', value);
+		this._updateFromPositionAndArea(this.position, this.area);
+	}
+
+	/**
+	 * Get the colWidth
+	 * @return {string} the coldWidth
+	 */
+	get colWidth(){return this._colWidth;}
+	/**
+	 * @param {string} value : the colWidth
+	 */
+	set colWidth(value){
+		if (this._colWidth === value) return;
+		this._colWidth = value;
+		this.setAttribute('colWidth', value);
+		this._updateFromPositionAndArea(this.position, this.area);
+	}
+
+	/**
+	 * Render the html
+	 */
 	render(){
 		return html`
 		<style>
@@ -83,14 +174,17 @@ export class MaskHighlighter extends HTMLElement {
 			</svg>
 		</div>
 		<svg class="mask-code">
-			<mask id="mask" x="0" y="0" width="100%" height="100%">
+			<mask id="mask" x="0" y="0" width="${this.widthParent}" height="${this.heightParent}">
 				<rect id="maskParent" x="0" y="0" width="${this.widthParent}" height="${this.heightParent}" class="parent" />
-				<rect id="maskHole" class="hole" style="width:${this.widthToUse}; height:${this.heightToUse};" x="${this.topToUse}" y="${this.leftToUse}" />
+				<rect id="maskHole" class="hole" style="width:${this.widthToUse}; height:${this.heightToUse};" x="${this.leftToUse}" y="${this.topToUse}" />
 			</mask>
 		</svg>
 		`;
 	}
 
+	/**
+	 * Invalidate the component and re-render it
+	 */
 	invalidate() {
 		if (!this.needsRender) {
 		  this.needsRender = true;
@@ -111,45 +205,91 @@ export class MaskHighlighter extends HTMLElement {
 		this.heightToUse = '0';
 		this.topToUse = '0';
 		this.leftToUse = '0';
+		// Default values
+		this.lineHeight = '1.15em';
+		this.colWidth = '35px';
 	}
 
 	connectedCallback() {
 		this.invalidate();
 	}
 
-	static get observerAttributes() {
-		return ['height', 'width', 'top', 'left', 'line', 'nbLines', 'col', 'nbCols', 'topMargin', 'leftMargin'];
+	static get observedAttributes() {
+		return [
+				// Area Attributes
+				'height',
+				'width',
+				'top',
+				'left',
+				// Position Attributes
+				'line',
+				'nbLines',
+				'col',
+				'nbCols',
+				'topMargin',
+				'leftMargin',
+				// Parameters Attributes
+				'lineHeight',
+				'colWidth'
+			];
 	}
 
-	attributeChangeCallback(attr, oldValue, newValue){
+	/**
+	 *
+	 * @param {string} attr
+	 * @param {string} oldValue
+	 * @param {string} newValue
+	 */
+	attributeChangedCallback(attr, oldValue, newValue){
 		switch(attr){
 			case 'height' :
 			case 'width' :
 			case 'top' :
 			case 'left' :
-				const areaTmp = area;
+				if (!this._area) this._area = {};
+				const areaTmp = this.area;
 				areaTmp[attr] = newValue;
-				area = areaTmp;
-			break;
-			case 'line' :
-			case 'nbLines' :
-			case 'col' :
-			case 'nbCols' :
-			case 'topMargin' :
-			case 'leftMargin' :
-				const positionTmp = position;
+				this.area = areaTmp;
+				break;
+				case 'line' :
+				case 'nbLines' :
+				case 'col' :
+				case 'nbCols' :
+				case 'topMargin' :
+				case 'leftMargin' :
+				if (!this._position) this._position = {};
+				const positionTmp = this.position;
 				positionTmp[attr] = newValue;
-				position = positionTmp;
+				this.position = positionTmp;
+			break;
+			case 'lineHeight':
+			case 'colWidth':
+				this[attr] = newValue;
 			break;
 		}
 	}
 
+	/**
+	 * process the modification of Area
+	 * @param {Area} areaNew
+	 */
 	_updateArea(areaNew) {
 		this._updateFromPositionAndArea(this.position, areaNew);
 	}
+
+	/**
+	 * Process the modification of Position
+	 * @param {Position} newPosition
+	 */
 	_updatePosition(newPosition) {
 		this._updateFromPositionAndArea(newPosition, this.area);
 	}
+
+	/**
+	 * Process the modification of position & area
+	 * @param {Position} position
+	 * @param {Area} area
+	 */
 	_updateFromPositionAndArea(position, area) {
 		const positionObject = {
 			width: 0,
@@ -157,6 +297,7 @@ export class MaskHighlighter extends HTMLElement {
 			top: 0,
 			left: 0
 		};
+		// We verify that position send are correct
 		if (this._validateTopFromPosition(position)) {
 			const margin = position.topMargin ? position.topMargin : 0;
 			positionObject.top = `calc(${margin} + (${this.lineHeight} * ${Math.max(0, position.line - 1)}))`;
@@ -181,30 +322,75 @@ export class MaskHighlighter extends HTMLElement {
 		}
 		this._changeProperties(positionObject);
 	}
+
+	/**
+	 * Validate the line attribute of the position attribute
+	 * @param {Position} position
+	 */
 	_validateTopFromPosition(position) {
 		return this.lineHeight && position && position.line != undefined;
 	}
+
+	/**
+	 * Validate the nbLines attribute of the position attribute
+	 * @param {Position} position
+	 */
 	_validateHeightFromPosition(position) {
 		return this.lineHeight && position && position.nbLines != undefined;
 	}
+
+	/**
+	 * Validate the col attribute of the position attribute
+	 * @param {Position} position
+	 */
 	_validateLeftFromPosition(position) {
 		return this.colWidth && position && position.col != undefined;
 	}
+
+	/**
+	 * Validate the nbCols attribute of the position attribute
+	 * @param {Position} position
+	 */
 	_validateWidthFromPosition(position) {
 		return this.colWidth && position && position.nbCols != undefined;
 	}
+
+	/**
+	 * Validate the top attribute of the area attribute
+	 * @param {Area} area
+	 */
 	_validateTopFromArea(area) {
 		return area && area.top != undefined;
 	}
+
+	/**
+	 * Validate the height attribute of the area attribute
+	 * @param {Area} area
+	 */
 	_validateHeightFromArea(area) {
 		return area && area.height != undefined;
 	}
+
+	/**
+	 * Validate the left attribute of the area attribute
+	 * @param {Area} area
+	 */
 	_validateLeftFromArea(area) {
 		return area && area.left != undefined;
 	}
+
+	/**
+	 * Validate the width attribute of the area attribute
+	 * @param {Area} area
+	 */
 	_validateWidthFromArea(area) {
 		return area && area.width != undefined;
 	}
+
+	/**
+	 * Apply all the changes to attributes to reflect on the dom
+	 * @param {Area} param
+	 */
 	_changeProperties({
 		width,
 		height,
@@ -216,22 +402,17 @@ export class MaskHighlighter extends HTMLElement {
 			this.maskHole = this.shadowRoot.querySelector('#maskHole');
 		}
 
+		// We fore the width & height of parent do to a rendering bug and to be shure that even the
+		// viewport change, the size of the component will follow thoses changes
 		this.widthParent = '100%';
 		this.heightParent = '100%';
-		//this.maskParent.setAttribute('width', '100%');
-		//this.maskParent.setAttribute('height', '100%');
+
+
 		this.widthToUse = isFinite(String(width)) ? `${width}px` : width;
 		this.heightToUse = isFinite(String(height)) ? `${height}px` : height;
 		this.topToUse = isFinite(String(top)) ? `${top}px` : top;
 		this.leftToUse = isFinite(String(left)) ? `${left}px` : left;
-		/*const widthToUse = isFinite(String(width)) ? `${width}px` : width;
-		const heightToUse = isFinite(String(height)) ? `${height}px` : height;
-		const topToUse = isFinite(String(top)) ? `${top}px` : top;
-		const leftToUse = isFinite(String(left)) ? `${left}px` : left;
-		/*this.maskHole.style.width = `${widthToUse}`;
-		this.maskHole.style.height = `${heightToUse}`;
-		this.maskHole.setAttribute('y', `${topToUse}`);
-		this.maskHole.setAttribute('x', `${leftToUse}`);*/
+		// Invalidate the template to force a re-rendering
 		this.invalidate();
 	}
 }
